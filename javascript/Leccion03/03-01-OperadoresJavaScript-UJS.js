dfd81cff4173b1938a90caa79456d7313b7156b4
Bruni Gabriel <gabybruni15@gmail.com>
2024-05-25 12:16:06 -0300

    7.5 Clase 7 en JavaScript realizada al día de la fecha

---
+++ javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+// Ejercicio; Para encontrar números pares e impares:
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
+
+// Ejercicio; Es mayor de edad:
+let edad = 18, adulto = 18;
+if( edad >= adulto ){
+    console.log("Usted es una persona adulta.");
+}
+else{
+    console.log("Usted es una persona menor de edad.");
+}
