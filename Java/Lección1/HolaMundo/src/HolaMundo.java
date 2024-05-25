71d474ce4c0a364389932754ad4a49dd956d4777
Bruni Gabriel <gabybruni15@gmail.com>
2024-05-25 17:38:56 -0300

    Operadores en Java Parte 1

--- Java/Lección1/HolaMundo/src/HolaMundo.java
+++ Java/Lección1/HolaMundo/src/HolaMundo.java
@@ -146,7 +146,7 @@ public class HolaMundo {
 //        System.out.println("edad = " + edad);
         
         // Conversión de tipos primitivos en Java Parte 2.
-        var edadTexto = String.valueOf(10);
+        /*var edadTexto = String.valueOf(10);
         System.out.println("edadTexto = " + edadTexto);
         
         var fraseChar = "programadores".charAt(4);
@@ -154,7 +154,49 @@ public class HolaMundo {
         
         System.out.println("Digite un caracter: ");
         fraseChar = entrada.nextLine().charAt(0);
-        System.out.println("fraseChar = " + fraseChar);
-              
-    }
+        System.out.println("fraseChar = " + fraseChar);*/
+        
+        /*int num1 = 5, num2 = 4;
+        var solucion = num1 + num2;
+        System.out.println("Solución de la suma es = " + solucion);
+        
+        solucion = num1 - num2;
+        System.out.println("Solución de la resta es = " + solucion);
+        
+        solucion = num1 * num2;
+        System.out.println("Solución de la multiplicación es = " + solucion);
+        
+        solucion = num1 / num2;
+        System.out.println("Solución de la división es = " + solucion);
+        
+        var solucion2 = 3.4 / num2;
+        System.out.println("Solución2 de la división es = " + solucion2);
+        
+        solucion = num1 % num2; // Guarda el residuo entero de la división.
+        System.out.println("solucion = " + solucion); // 5/4 
+        
+        if (num2 % 2 == 0)
+            System.out.println("Es un número PAR.");
+        else
+            System.out.println("Es un número IMPAR.");*/
+        
+        int varNum1 = 1, varNum2 = 4;
+        var varNum3 = varNum1 + 6 - varNum2; // Una operación.
+        System.out.println("varNum3 = " + varNum3);
+        
+        varNum1 += 1; // varNum1 = var Num1 + 1;
+        System.out.println("varNum1 = " + varNum1);
+        
+// -=    *=    /=    %=
+        varNum2 -= 2;
+        System.out.println("varNum2 = " + varNum2);
+        varNum1 *= 5;
+        System.out.println("varNum1 = " + varNum1);
+        varNum3 /= 4;
+        System.out.println("varNum3 = " + varNum3);
+        varNum1 %= 6;
+        System.out.println("varNum1 = " + varNum1);
+        
+        
+    }    
 }
