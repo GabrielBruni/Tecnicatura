8abe355c1adac65e6693c655cadd35e3aa9e0c9e
Bruni Gabriel <gabybruni15@gmail.com>
2024-06-25 09:37:51 -0300

    Clase 11 Java Lunes 24/06/24 Ejercicio N°6 Condicionales

--- Java/Lección1/CondicionalEjercicio_6/src/Ejercicio6/Ejercicio6.java
+++ Java/Lección1/CondicionalEjercicio_6/src/Ejercicio6/Ejercicio6.java
@@ -10,5 +10,23 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
+        System.out.println("Digite el primer número: ");
+        int num1 = Integer.parseInt(entrada.nextLine());
+        
+        System.out.println("Digite el segundo número: ");
+        int num2 = Integer.parseInt(entrada.nextLine());
+        
+        int resultado = 0;
+        
+        if (num1 > num2){
+            resultado = num1 - num2;
+        }
+        else if (num1 < num2){
+            resultado = num1 + num2;
+        }
+        else{
+            resultado = num1 * num2;
+        }
+        System.out.println("El resultado es: "+resultado);
     }
 }
