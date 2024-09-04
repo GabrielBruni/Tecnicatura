1b442a2647a0ea629acc46bc24b134fbc550ab5d
Bruni Gabriel <gabybruni15@gmail.com>
2024-09-04 20:34:27 -0300

    Comiteamos leccion 3 clases y objetos en Java Miércoles 04/09/24

--- Java/Lección3/LecciónClasesObjetos/src/Clases/Persona.java
+++ Java/Lección3/LecciónClasesObjetos/src/Clases/Persona.java
@@ -2,7 +2,13 @@
 package Clases;
 
 public class Persona {
-    // Atributis de la clase. (Características)
+    // Atributis de la clase. (Características).
     String nombre; 
     String apellido;
+    
+    // Métodos de la clase. (Acciones).
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
---
+++ Java/Lección3/LecciónClasesObjetos/src/Clases/PruebaPersona.java
@@ -0,0 +1,19 @@
+
+package Clases;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona(); // Llamamos al constructor.
+        persona1.nombre = "Gabriel"; // El valor hexadecimal norlmalmente comienza con Ox.
+        persona1.apellido = "Bruni";
+        persona1.obtenerInformacion();
+        
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
+        System.out.println("persona1 = " + persona1);
+        persona2.obtenerInformacion();
+        persona2.nombre = "Osvaldo";
+        persona2.apellido = "Giordanini";
+        persona2.obtenerInformacion();
+    }
+}
