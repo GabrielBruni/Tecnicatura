2380ee5664797da92560fd00136bd4e0f7363f4b
GabrielBruni <166764577+GabrielBruni@users.noreply.github.com>
2024-09-26 09:42:55 -0300

    Commit de Java Clase 07 en POO Parte 5
    Encapsulamiento, Clases y Objetos.

--- Java/Lección4/Aritmética/src/Operaciones/PruebaAritmetica.java
+++ Java/Lección4/Aritmética/src/Operaciones/PruebaAritmetica.java
@@ -4,13 +4,13 @@ package Operaciones;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         var a = 10; // Variables locales.
-        int b = 7; // 
+        int b = 7; // Memoria stack.
         miMetodo(); // Llamamos el nuevo método.
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
         aritmetica1.sumarNumeros();
-        
+        // Para almacenar un objeto o los atributos se utiliza la memoria heap.
         int resultado = aritmetica1.sumarConRetorno();
         System.out.println("resultado = " + resultado);
                 
@@ -23,10 +23,41 @@ public class PruebaAritmetica {
         Aritmetica aritmetica2 = new Aritmetica(5, 8);
         System.out.println("aritmetica2 = " + aritmetica2.a);
         System.out.println("aritmetica2 = " + aritmetica2.b );
+        //aritmetica1 = null; nunca utilizar esto, no se debe hacer.
+        //System.gc(); método para limpiar residuos, es pesado, no utilizar.
+        Persona persona = new Persona("Ariel", "Betancud");
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombre: " + persona.nombre);
+        System.out.println("Persona apellido: " + persona.apellido); 
     }
-    
+    // Modularidad creamos un nuevo método.
     public static void miMetodo(){
         // a = 10; // Una variable está limitada.
         System.out.println("Aquí hay otro método");
+    }
+}
+// Creamos una nueva clase.
+class Persona{
+    String nombre;
+    String apellido;
+    
+    Persona(String nombre, String apellido){ // Constructor.
+        super(); // Llamada al contructor de la clase Padre Object.
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        System.out.println("Objeto persona usando this: "+this);
+    }
+}
+
+class Imprimir{
+    public Imprimir(){
+        super(); // El contructor de la clase padre, para reservar memoria.
+    }
+    
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde la clase imprimir: " +persona);
+        System.out.println("Impresión del objeto actual (this): "+this);
     }
 }
