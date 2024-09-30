d3adbdba03f6b877c96bbbe51c4928d27352526a
GabrielBruni <166764577+GabrielBruni@users.noreply.github.com>
2024-09-30 20:59:54 -0300

    Cambiando de ramas

--- Python/Leccion6/Persona.py
+++ Python/Leccion6/Persona.py
@@ -4,7 +4,7 @@ class Persona: # Creamos una clase.
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
-    def mostrar_detalle(self):
+    def mostrar_detalle(self): # self es igual a this.
         print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
 
 persona1 = Persona('Gabriel', 'Bruni', 24) # Necesitamos enviar argumentos.
@@ -19,9 +19,15 @@ print(f'El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido} Su
 persona1.nombre = 'Liliana'
 persona1.apellido = 'Bucella'
 persona1.edad = 40
-print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}'
+print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
 
 # Los atributos son: características.
 # Los métodos son: el comportamiento que van a tener los objetos (acciones).
-persona1.mostrar_detalle()
+persona1.mostrar_detalle() # La referencia en este caso se pasa de manera automática.
 persona2.mostrar_detalle()
+
+#Persona.mostrar_detalle(persona1) # Debemos pasarle una referencia para el self o dará error.
+persona1.telefono = '44445555289'
+print(f'Este es el teléfono de: {persona1.nombre} {persona1.telefono}') # Hemos creado un atributo de un objeto.
+
+# print(persona2.telfono) # El objeto persona2 no tiene ese atributo, da error.
